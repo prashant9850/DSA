ed58ad3fa02337808e4e384a4847ca558ab3be27
prashant9850 <prashantsureshjadhav409@gmail.com>
2025-07-20 23:55:02 +0530

    testing git link is working

---
+++ inverse.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int num, reverse = 0;
+    cout<<"enter a number : ";
+    cin >> num;
+    while(num != 0) {
+        reverse = reverse * 10 + num % 10;
+        num /= 10;
+    }
+    cout << reverse << endl;
+    return 0;
+}
