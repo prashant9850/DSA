43b84bb9fc8ff186e965ca0ebf5e404bbd880989
prashant9850 <prashantsureshjadhav409@gmail.com>
2025-07-30 23:39:18 +0530

    pattern day4

--- pattern/triangle.cpp
+++ pattern/triangle.cpp
@@ -7,12 +7,9 @@ int main() {
     cin >> n;
 
     for (int i = 0; i < n; i++) {
-        // Print spaces
         for (int j = 0; j < n - i - 1; j++) {
             cout << " ";
         }
-
-        // Print stars
         for (int k = 0; k < (2 * i + 1); k++) {
             cout << "*";
         }
